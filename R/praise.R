169eb8b81241243027dce9a79063a0dc6af1a637
Gabriela DSouza <g.dsouza0191@gmail.com>
2018-11-21 15:24:16 +1100

    This commit will initialise the git repo, the first push of the praiseme package

---
+++ R/praise.R
@@ -0,0 +1,9 @@
+#'@title Praise someone
+#'@description giving people praise
+#'@param name name of praise recipient
+#'@examples praise("Broccoli")
+#'@export
+
+praise<-function(name='Gaby'){
+  paste("you are incredible,", name, "!")
+  }
